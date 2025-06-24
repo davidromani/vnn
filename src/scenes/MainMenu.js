328299a1583f0f9605c7db2066ff0b1f7f80d03a
David Romaní <david@flux.cat>
2025-06-24 09:54:01 +0200

    add some UI layout improvements

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -11,18 +11,18 @@ export class MainMenu extends Scene
     {
         this.add.image(512, 384, 'background');
 
-        this.add.image(512, 300, 'logo');
+        //this.add.image(512, 300, 'logo');
 
-        this.add.text(512, 460, 'Main Menu', {
+        this.add.text(512, 60, 'VNN', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
+            stroke: '#000000', 
+            strokeThickness: 2,
             align: 'center'
         }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
-
+        /*this.input.once('pointerdown', () => {
             this.scene.start('Game');
-
-        });
+        });*/
+        //this.scene.start('Game');
     }
 }
