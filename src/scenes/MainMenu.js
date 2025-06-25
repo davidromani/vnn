126887277dc5a7fb49c9d9bc59accc70c849af31
David Romaní <david@flux.cat>
2025-06-25 09:31:39 +0200

    remove assets/bg.png

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -9,17 +9,16 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.add.image(512, 384, 'background');
-
-        //this.add.image(512, 300, 'logo');
-
-        this.add.text(512, 60, 'VNN', {
+        const centerX = this.cameras.main.width / 2;
+        const centerY = this.cameras.main.height / 2;
+        //this.add.image(512, 384, 'background');
+        this.add.text(centerX, centerY, 'VNN', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', 
-            strokeThickness: 2,
+            strokeThickness: 3,
             align: 'center'
         }).setOrigin(0.5);
-
+        // TODO add main menu info to display and wait a click to start
         /*this.input.once('pointerdown', () => {
             this.scene.start('Game');
         });*/
