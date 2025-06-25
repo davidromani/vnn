ffa845641291fb2073342e32c77decde350d823e
David Romaní <david@flux.cat>
2025-06-25 17:57:00 +0200

    ignore dist.zip

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -6,9 +6,7 @@ export class Preloader extends Scene {
     }
 
     init() {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        // this.add.image(512, 384, 'background');
-        //  A simple progress bar. This is the outline of the bar.
+        // A simple progress bar. This is the outline of the bar.
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
         const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
         this.load.on('progress', (progress) => {
@@ -30,10 +28,9 @@ export class Preloader extends Scene {
     }
 
     create() {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
-
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+        // When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
+        // For example, you can define global animations here, so we can use them in other scenes.
+        // Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
         this.scene.start('MainMenu');
     }
 }
